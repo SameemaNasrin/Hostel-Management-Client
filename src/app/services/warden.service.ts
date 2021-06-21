7c460036f36b8b15a04b0050504f73dfdfc16bab
Shadraza786 <razashadman786@gmail.com>
2021-06-21 17:00:04 +0530

    viewallwarden

--- src/app/services/warden.service.ts
+++ src/app/services/warden.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { HttpClient} from '@angular/common/http';
+import { Observable} from 'rxjs';
+import { Wardendto } from '../dto/wardendto';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WardenService {
-
-  constructor() { }
+  viewallwarden():Observable<any>{
+    return this.http.get("http://localhost:8082/warden/get")
+  }
+  constructor(public http:HttpClient) { }
 }
