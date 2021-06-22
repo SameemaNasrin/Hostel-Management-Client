24746045a8ea9cf346a10c0fdf4488a05f0fa238
Shadraza786 <razashadman786@gmail.com>
2021-06-23 00:09:13 +0530

    changes

--- src/app/entities/warden.ts
+++ src/app/entities/warden.ts
@@ -5,5 +5,5 @@ export class Warden {
     id: number;
     name: string;
     email: string;
-    hostel:Hostel; //hostel entity needs to be created
+    hostel:Hostel; 
 }
--- src/app/services/warden.service.ts
+++ src/app/services/warden.service.ts
@@ -23,5 +23,9 @@ export class WardenService {
     return this.http.get(Constants.baseUrl + `${this.prefix}get/${wid}`);
   }
 
+  viewByHId (hid: number): Observable<any> {
+    return this.http.get(Constants.baseUrl + `${this.prefix}get/${hid}`);
+  }
+
 }
 
