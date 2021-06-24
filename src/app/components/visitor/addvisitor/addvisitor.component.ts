8b6c40fd8cc53938e1869bd3dd1a95cc20f729bd
supriyo15998 <supriyo15998@gmail.com>
2021-06-24 10:11:32 +0530

    add visitor completed

--- src/app/components/visitor/addvisitor/addvisitor.component.ts
+++ src/app/components/visitor/addvisitor/addvisitor.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Visitordto } from 'src/app/dto/visitordto';
+import { Hostel } from 'src/app/entities/hostel';
+import { Student } from 'src/app/entities/student';
+import { HostelService } from 'src/app/services/hostel.service';
+import { StudentService } from 'src/app/services/student.service';
+import { VisitorService } from 'src/app/services/visitor.service';
 
 @Component({
   selector: 'app-addvisitor',
@@ -6,10 +13,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./addvisitor.component.css']
 })
 export class AddvisitorComponent implements OnInit {
+  responseMsg: string;
+  errorMsgs = []
+  visitorDto: Visitordto = new Visitordto();
+  students: Student[]
+  hostels: Hostel[]
+  @ViewChild("addVisitorForm")
+  private form: NgForm
+  constructor(private studentService: StudentService, private hostelService: HostelService, private visitorService: VisitorService) { }
 
-  constructor() { }
+  ngOnInit () {
+    this.studentService.getStudents().subscribe(
+      data => {
+        this.students = data;
 
-  ngOnInit() {
+      },
+      error => {
+        console.log(error);
+
+      }
+    );
+    this.hostelService.viewAll().subscribe(
+      data => {
+        this.hostels = data
+      },
+      error => {
+        console.log(error);
+
+      }
+    )
+  }
+
+  addVisitor (): void {
+    this.errorMsgs = []
+    this.visitorService.addVisitor(this.visitorDto).subscribe(
+      data => {
+        this.form.reset()
+        this.responseMsg = data.message
+
+      },
+      error => {
+        error.error.messages.forEach(element => {
+          this.errorMsgs.push(element)
+        });
+
+      }
+    )
   }
 
 }
--- src/app/dto/visitordto.ts
+++ src/app/dto/visitordto.ts
@@ -1,12 +1,11 @@
 export class Visitordto {
-    id:number;
-    name:string;
-    contactNumber:number;
-    studentId:number;
-    studentRelation:string;
-    visitorAddress:string;
-    reason:string;
-    dateOfVisiting:string;
-    hostelId:number;
+    name: string;
+    number: number;
+    studentId: number;
+    studentRelation: string;
+    visitorAddress: string;
+    reason: string;
+    dateOfVisiting: string;
+    hostelId: number;
 }
 
