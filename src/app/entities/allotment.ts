2bfe1d5de92d89ca2cf02617bba3c2b99abbb1dd
Qaisar786git <userqrk@gmail.com>
2021-06-25 17:19:28 +0530

    allo fix

--- src/app/entities/allotment.ts
+++ src/app/entities/allotment.ts
@@ -1,8 +1,10 @@
 import { Hostel } from "./hostel";
+import { Room } from "./room";
 import { Student } from "./student";
 
 export class Allotment {
     id: number;
+    room:Room;
     student:Student;
     hostel:Hostel; 
 }
