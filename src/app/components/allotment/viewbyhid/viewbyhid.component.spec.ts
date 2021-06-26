ba67be378bca5f698990b3f40857095bdd1fb84a
Qaisar786git <userqrk@gmail.com>
2021-06-26 22:29:57 +0530

    allo

--- src/app/components/allotment/viewbyhid/viewbyhid.component.spec.ts
+++ src/app/components/allotment/viewbyhid/viewbyhid.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ViewbyhidComponent } from './viewbyhid.component';
 
 describe('ViewbyhidComponent', () => {
--- src/app/components/allotment/viewbyhid/viewbyhid.component.ts
+++ src/app/components/allotment/viewbyhid/viewbyhid.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Allotment } from 'src/app/entities/allotment';
 import { AllotmentserviceService } from 'src/app/services/allotmentservice.service';
 
 @Component({
@@ -7,17 +8,41 @@ import { AllotmentserviceService } from 'src/app/services/allotmentservice.servi
   styleUrls: ['./viewbyhid.component.css']
 })
 export class ViewbyhidComponent implements OnInit {
-  hostelId:number;
-  allotment:any = undefined;
-  msg:string;
+
+  allotment:Allotment[]
+  errorMsgs = []
+  id: number;
+  //hid:number;
   constructor(public allotmentservice:AllotmentserviceService) { }
+  
 
   ngOnInit() {
   }
-  viewallotmentbyhid():void{
-    if (this.hostelId==undefined || this.hostelId==null  || this.hostelId<=0){
-      this.msg="Enter the Hostel ID greater than 0";
-      return ;
+ 
+  viewallotmentbyhid() {
+    console.log(this.id);
+    if (this.id == undefined || this.id == null || this.id <= 0) {
+      this.errorMsgs[0] = "Enter the Hostel ID greater than 0"
+      return;
     }
+    this.allotmentservice.viewallotmentbyhid(this.id).subscribe(
+      data => {
+        console.log(data);
+        this.allotment = [];
+        data.forEach(e => {
+          this.allotment.push(e);
+        });
+          //this.wardens.push(data);
+        console.log(this.allotment);
+        this.errorMsgs = undefined;
+      },
+      error => {
+        console.log(error);
+        this.errorMsgs = error.error.messages;
+        console.log(this.errorMsgs);
+
+      }
+    )
   }
+
 }
